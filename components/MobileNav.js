d48b05aada7cd4ba2860cadbb763551f93add86d
wenzhi.li <wenzhi.li@verbinteractive.com>
2021-06-11 14:56:11 -0300

    fix side menu z-index

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -47,7 +47,7 @@ const MobileNav = () => {
         </svg>
       </button>
       <div
-        className={`fixed w-full h-full top-24 right-0 bg-gray-200 dark:bg-gray-800 opacity-95 z-10 transform ease-in-out duration-300 ${
+        className={`z-20 fixed w-full h-full top-24 right-0 bg-blue-400 dark:bg-gray-800 opacity-95 transform ease-in-out duration-300 ${
           navShow ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
@@ -57,9 +57,10 @@ const MobileNav = () => {
           className="fixed w-full h-full cursor-auto focus:outline-none"
           onClick={onToggleNav}
         ></button>
-        <nav className="fixed h-full mt-8">
+        <nav className="fixed h-full w-full">
           {headerNavLinks.map((link) => (
-            <div key={link.title} className="px-12 py-4">
+            <div key={link.title} 
+              className="text-right px-12 py-4 border-white border border-l-0 border-r-0 border-t-0">
               <Link
                 href={link.href}
                 className="text-2xl font-bold tracking-widest text-gray-900 dark:text-gray-100"
